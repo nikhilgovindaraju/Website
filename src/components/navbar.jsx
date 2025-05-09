2fcd63dd9457bcdb4d65188a72c9cc4649b2c64b
NikhilGovind <ngovinda@usc.edu>
2025-05-08 20:15:09 -0700

    fixed responsiveness on mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import styles from "../styles/navbar.module.css"; // make sure path matches
 
 const Navbar = () => {
     return (
-        <nav>
-            <a href="#hero">Home</a>
-            <a href="#about">About</a>
-            <a href="#experience">Experience</a>
-            <a href="#projects">Projects</a>
-            <a href="#skills">Skills</a>
-            <a href="#contact">Contact</a>
-        </nav>
+        <div className={styles.navbar}>
+            <nav>
+                <ul>
+                    <li><a href="#hero">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 
