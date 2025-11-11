1a71c805c915e106d58cf339c53c47fbddecd715
nikhilgovindaraju <nikhilgovindraju@gmail.com>
2025-11-10 21:19:57 -0800

    Updated resume, hero page, skills section and also changes the contact page

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            &copy; {new Date().getFullYear()} Nikhil Govindaraju. All rights reserved.
-        </footer>
-    );
-}
-
-export default Footer;
